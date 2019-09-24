b48e7ffc3daaa63d938dbbcb3207b46ae37e9981
linqueta <lincolnrodrs@gmail.com>
2019-09-24 17:03:13 -0300

    change db credentials

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :my_api, MyApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "linqueta",
+  password: "linqueta",
   database: "my_api_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
