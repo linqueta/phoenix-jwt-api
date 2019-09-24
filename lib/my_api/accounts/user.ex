28f7e8e486451c454ae3e8005f4bbb98211ec1d2
linqueta <lincolnrodrs@gmail.com>
2019-09-24 17:19:02 -0300

    set password salt for user

--- lib/my_api/accounts/user.ex
+++ lib/my_api/accounts/user.ex
@@ -1,11 +1,13 @@
 defmodule MyApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias MyApi.Accounts.User
+
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
 
   schema "users" do
     field :email, :string
     field :password_hash, :string
-    # Virtual fields:
     field :password, :string, virtual: true
     field :password_confirmation, :string, virtual: true
 
@@ -13,13 +15,25 @@ defmodule MyApi.Accounts.User do
   end
 
   @doc false
-  def changeset(user, attrs) do
+  def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:email, :password, :password_confirmation]) # Remove hash, add pw + pw confirmation
-    |> validate_required([:email, :password, :password_confirmation]) # Remove hash, add pw + pw confirmation
-    |> validate_format(:email, ~r/@/) # Check that email is valid
-    |> validate_length(:password, min: 8) # Check that password length is >= 8
-    |> validate_confirmation(:password) # Check that password === password_confirmation
+    |> cast(attrs, [:email, :password, :password_confirmation])
+    |> validate_required([:email, :password, :password_confirmation])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 8)
+    |> validate_confirmation(:password)
     |> unique_constraint(:email)
+    |> put_password_hash
   end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+        ->
+          put_change(changeset, :password_hash, hashpwsalt(pass))
+      _ ->
+          changeset
+    end
+  end
+
 end
