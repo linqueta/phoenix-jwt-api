fca7f7baf6cdd0f9557290adafb326a0a1a7f969
linqueta <lincolnrodrs@gmail.com>
2019-09-24 18:57:25 -0300

    add sign in and my user data

--- lib/my_api_web/router.ex
+++ lib/my_api_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MyApiWeb.Router do
   use MyApiWeb, :router
 
+  alias MyApi.Guardian
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -11,6 +13,10 @@ defmodule MyApiWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/", MyApiWeb do
@@ -25,6 +31,12 @@ defmodule MyApiWeb.Router do
 
     post "/sign_up", UserController, :create
     post "/sign_in", UserController, :sign_in
+  end
+
+  scope "/api/v1", MyApiWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
   end
 
   # Other scopes may use custom stacks.
